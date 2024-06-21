be2738c6cf61491f25376689aad05b7ca28ac837
Natnael Mehari <natnaeltsg27@gmail.com>
2024-06-21 19:56:10 +0000

    add the start message function first

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+def start_message():
+    """"
+    create the start message for the Battleships playground
+    """
+    print("~" * 27)
+    print("WELCOME TO BATTLESHIPS\n")
+    print("There is a battleship hidden awaiting for you. come and try to sink it!")
+    print("~" * 27)
+
+start_message()
