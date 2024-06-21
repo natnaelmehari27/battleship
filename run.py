9f9e741407b8362b5f9f071ec0fa60d33f836cf2
Natnael Mehari <natnaeltsg27@gmail.com>
2024-06-21 20:01:43 +0000

    create the battleship square and remove the string from the board

--- run.py
+++ run.py
@@ -10,3 +10,18 @@ def start_message():
     print("~" * 27)
 
 start_message()
+
+def set_board(size):
+    """
+    Create the square playground for the Battleship game
+    """
+    return[['.' for count in range(size)] for count in range(size)]
+
+set_board
+
+def print_board(board):
+    for b in board:
+        print(*b)
+
+board = set_board(4)
+print_board(board)
