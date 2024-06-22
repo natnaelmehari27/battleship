188dbc20d4d327ca1bf9c74d309f3d7867ceaa26
Natnael Mehari <natnaeltsg27@gmail.com>
2024-06-22 05:27:58 +0000

    add a play again function for the end of the game

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ def start_message():
     print("WELCOME TO BATTLESHIPS\n")
     print("There is a battleship hidden awaiting for you. come and try to sink it!")
     print("~" * 27)
+    print("first start by hiding your battleship and then start hitting our enemy.\n")
 
-start_message()
 
 def set_board(size):
     """
@@ -17,14 +17,12 @@ def set_board(size):
     """
     return[['.' for count in range(size)] for count in range(size)]
 
-set_board
+
 
 def print_board(board):
     for b in board:
         print(*b)
 
-board = set_board(4)
-print_board(board)
 
 def set_ship(size):
     """
@@ -44,8 +42,6 @@ def set_ship(size):
         coords = tuple(zip(row_ship, col_ship))
     return list(coords)
 
-ship = set_ship(2); ship
-
 def make_guess():
     """
     Convert all strings values into integers
@@ -53,7 +49,7 @@ def make_guess():
     row = int(input('row: ')) - 1
     col = int(input('col: ')) - 1
     return (row, col)
-value = make_guess(); value
+
 
 def update_board(guess, board, ship, guesses):
     if guess in guesses:
@@ -68,19 +64,44 @@ def update_board(guess, board, ship, guesses):
         print("Opps! You have missed.")
     return board
 
-guesses = []
-our_guess = make_guess()
-board = update_board(our_guess, board, ship, guesses)
-print_board(board)
+
+
+def play_again():
+    """
+    To ask the player if they want to play again.
+    """
+    print("Choose an option:\n1 - yes\n2 - no")
+    while(True):
+        try:
+            answer = int(
+                input("Play again? "))
+            if(answer == 1):
+                main()
+                return False 
+            elif (answer == 2):
+                print("Thanks for playing!")
+                return False
+            else:
+                print("Not a valid choice, choose 1 or 2.")
+        except ValueError:
+            print("Not an integer, please try again")
+
 
 def main():
+    start_message()
     board = set_board(4)
+    print_board(board)
     ship = set_ship(2)
+    value = make_guess(); value
     guesses = []
     while len(ship) > 0:
         board = update_board(make_guess(), board, ship, guesses)
         print_board(board)
     print('You sunk my battleship!')
-    return 
+    play_again()
+    guesses = []
+    our_guess = make_guess()
+    board = update_board(our_guess, board, ship, guesses)
+    print_board(board)
 
 main()
